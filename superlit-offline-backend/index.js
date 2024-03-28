a484317dcd9746ce6366f29b7127c51be21d4c7c
Vyoman Jain <75789232+VyoJ@users.noreply.github.com>
2024-03-28 09:05:09 +0530

    Test, class routes

--- superlit-offline-backend/index.js
+++ superlit-offline-backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const demoRoutes = require("./routes/demoRoutes");
 const userRoutes = require("./routes/userRoutes");
@@ -10,25 +10,24 @@ const PORT = 6969;
 
 app.use(express.json());
 app.use(
-    cors({
-        origin: ["http://localhost:3000"],
-        credentials: true,
-    }),
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
 );
 
-const connectToDb = async() => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log("Connected to DB");
-    }
-    catch (error) {
-        console.log(error);
-    }
+const connectToDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to DB");
+  } catch (error) {
+    console.log(error);
+  }
 };
 connectToDb();
 
 app.use(bodyParser.json());
 app.use("/", demoRoutes);
-app.use("/", demoRoutes);
 app.use("/auth", userRoutes);
+app.use("/test", testRoutes);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- superlit-offline-backend/routes/testRoutes.js
+++ superlit-offline-backend/routes/testRoutes.js
@@ -1,17 +1,53 @@
 const express = require("express");
 const router = express.Router();
 const Test = require("../schemas/testSchema");
+const Question = require("../schemas/questionSchema");
 
 router.get("/get_test_data/:test_id", async (req, res) => {
-  // fetch test data from db
-  const test_data = await Test.findOne({
-    _id: Number(req.params.test_id),
-  });
-  console.log(test_data);
-  res.send(test_data);
+  try {
+    let test_data = await Test.findById(req.params.test_id);
+    console.log(test_data);
+    res.send(test_data).status(200);
+  } catch (error) {
+    console.log(error);
+    res.status(404).send("Could not find given test");
+  }
 });
 
-// router.post("/create_test", async (req, res) => {
-// });
+router.get("/get_all_tests/:class_id", async (req, res) => {
+  try {
+    let test_data = await Test.find({ classId: req.params.class_id });
+    console.log(test_data);
+    res.send(test_data).status(200);
+  } catch (error) {
+    console.log(error);
+    res.status(404).send("Could not find any tests for given teacher");
+  }
+});
+
+router.post("/create_test", async (req, res) => {
+  try {
+    let test_data = req.body;
+    let test = new Test({ test_data });
+    let saved_test = await test.save();
+    console.log(saved_test);
+    try {
+      for (let questionData of test_data.questions) {
+        let question = new Question({
+          ...questionData,
+          testId: saved_test._id,
+        });
+        await question.save();
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Something went wrong while creating the questions");
+    }
+    res.send(saved_test).status(201);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Could not save the given test");
+  }
+});
 
 module.exports = router;
